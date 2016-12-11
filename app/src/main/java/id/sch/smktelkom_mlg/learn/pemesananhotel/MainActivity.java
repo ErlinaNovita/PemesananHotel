f91985a2f62d567bb9253727c71cf413a70a35be
ErlinaNovita <puterierlina99@gmail.com>
2016-12-11 17:04:20 +0700

    EditText TextView Button

--- app/src/main/java/id/sch/smktelkom_mlg/learn/pemesananhotel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/pemesananhotel/MainActivity.java
@@ -2,11 +2,57 @@ package id.sch.smktelkom_mlg.learn.pemesananhotel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    Button bOK;
+    TextView tvHasil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doProcess();
+            }
+
+
+        });
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            tvHasil.setText("saya atas nama " + nama + " memesan");
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 5) {
+            etNama.setError("Nama minimal 5 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        return valid;
     }
 }
