ad88342ebefd78625bf0bcb1b73e5e1bc4da8db6
ErlinaNovita <puterierlina99@gmail.com>
2016-12-11 22:41:04 +0700

    CheckButton

--- app/src/main/java/id/sch/smktelkom_mlg/learn/pemesananhotel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/pemesananhotel/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvHasil;
     RadioButton rbGold, rbSilver;
     Spinner spService;
+    CheckBox cbGold, cbPastel, cbWhite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         spService = (Spinner) findViewById(R.id.spinnerService);
 
+        cbGold = (CheckBox) findViewById(R.id.checkBoxGold);
+        cbPastel = (CheckBox) findViewById(R.id.checkBoxPastel);
+        cbWhite = (CheckBox) findViewById(R.id.checkBoxWhite);
+
         etNama = (EditText) findViewById(R.id.editTextNama);
         bOK = (Button) findViewById(R.id.buttonOK);
 
@@ -36,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 doProcess();
+
             }
         });
         findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
@@ -58,9 +65,20 @@ public class MainActivity extends AppCompatActivity {
             tvHasil.setText("Belum memilih status");
         }
 
+        String a = "";
+        if (cbGold.isChecked()) {
+            a += "Nuansa Gold";
+        }
+        if (cbPastel.isChecked()) {
+            a += "Nuansa Pastel";
+        }
+        if (cbWhite.isChecked()) {
+            a += "Nuansa White";
+        }
+
         if (isValid()) {
             String nama = etNama.getText().toString();
-            tvHasil.setText("saya atas nama " + nama + " memesan paket " + hasil + " dengan tambahan service " + spService.getSelectedItem().toString());
+            tvHasil.setText("saya atas nama " + nama + " memesan paket " + hasil + " dengan tambahan service " + spService.getSelectedItem().toString() + " memilih ruangan dengan " + a);
         }
     }
 
